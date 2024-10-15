925421cec4740b5ad108755f600ffe8fbd1bb783
JessicaTemian <B00153952@mytudublin.ie>
2024-10-15 15:46:43 +0000

    part2done

---
+++ newBooking.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <form method="POST" action="createNewBooking.php">
+            <?PHP include ("createMemberDropDown.php");?>
+
+            Member ID: <input type="text" name="memberID"/><br>
+            Court ID: <input type="text" name="courtID"/><br>
+            Booking Date: <input type="date" name="bookingDate"/><br>
+            Start Time: <input type="time" name="startTime"/><br>
+            End Time: <input type="time" name="endTime"/><br>
+            <input type="submit">
+        </form>
+        
+    </body>
+</html>
